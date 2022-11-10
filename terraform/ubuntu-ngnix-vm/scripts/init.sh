9d035c7a6b37c7bf0157b72c08f73a21ed72fdfc
Hrvoje Tonkovac <hrvoje.tonkovac@toptal.com>
2022-11-10 10:40:26 +0100

    x

--- terraform/ubuntu-ngnix-vm/scripts/init.sh
+++ terraform/ubuntu-ngnix-vm/scripts/init.sh
@@ -19,3 +19,4 @@ sudo usermod -a -G docker $USER
 sudo systemctl enable docker
 sudo systemctl restart docker
 sudo docker run --name docker-nginx -p 80:80 nginx:latest
+sudo usermod -a -G docker adminuser
